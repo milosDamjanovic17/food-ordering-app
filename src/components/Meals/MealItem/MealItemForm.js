e9cca10f1d11f2d762d4e742e779a4f4813e5c8b
Milos Damjanovic <milos.10001@yahoo.com>
2023-04-17 19:33:00 -0400

    finished meal item form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+import Input from "../../UI/Input";
 import styles from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
   return (
     <form className={styles.form}>
-      <input />
-      <button>Add</button>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
     </form>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -4,7 +4,7 @@ const Input = (props) => {
   return (
     <div className={styles.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input />
+      <input id={props.input.id} {...props.input} />
     </div>
   );
 };
